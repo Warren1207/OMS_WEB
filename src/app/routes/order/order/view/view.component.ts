981190daf5fb100ba434ac8be4841fbeb4a9b4af
Warren Lee <913932075@qq.com>
2018-07-14 18:04:50 +0800

    order

--- src/app/routes/order/order/view/view.component.ts
+++ src/app/routes/order/order/view/view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
+import { _HttpClient } from '@delon/theme';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from '../../order.service';
+
 
 @Component({
   selector: 'app-view',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ViewComponent implements OnInit {
-
-  constructor() { }
+  base: any;
+  detail: any;
+  id: any = this.activateInfo.snapshot.params['id'];
+  
+  constructor(
+    public http: _HttpClient,
+    private activateInfo: ActivatedRoute,
+    private service: OrderService) { }
 
   ngOnInit() {
+    this.service.getFn(`get/`+this.id).subscribe(function(res){
+      this.base = res;
+      if(this.base.NUMBER){
+        this.service.getFn('getDetail/'+this.base.NUMBER).subscribe(res => this.detail = res);
+      }
+    }.bind(this));
   }
 
 }
